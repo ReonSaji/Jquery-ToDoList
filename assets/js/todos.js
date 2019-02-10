a3d53428e10d543a849079281e980b956398f075
Reon Saji <reonsajinedz@gmail.com>
2019-02-10 16:49:36 +0530

    V1

---
+++ assets/js/todos.js
@@ -0,0 +1,44 @@
+//Check off specific todos by clicking
+//Completed List
+
+$('ul').on("click", '.p', function () {
+  $(this).parent().toggleClass('completed');
+}); 
+
+
+//click on X to delete
+
+$('ul').on('click', '.fa', function (event) {
+  $(this).parent().parent().fadeOut(500, function () {
+    $(this).remove();
+  })
+  event.stopPropagation();
+});
+
+//Add Todo
+
+$("input").on("keypress", function (event) {
+  if (event.which === 13) {
+
+    //grabbing new todo text from input
+    var todoText = $(this).val();
+    $(this).val("");
+
+    //create a new li and add ul
+    $('ul').append(`<li>
+    <span class="p">${todoText}</span>
+    <span><i class="fa fa-trash-alt"></i></span>
+  </li>`);
+  }
+});
+
+
+
+
+$('.fa-plus').click(function () {
+  $("input[type='text']").fadeToggle();
+});
+
+
+
+
